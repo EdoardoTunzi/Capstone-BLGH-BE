30a954f835f0a94f3ce80e85db72bb8cbc198dcf
Edoardo <edfl.tunzi@gmail.com>
2025-03-03 10:00:43 +0100

    initial commit

---
+++ src/main/java/com/example/Capstone_BLGH_BE/CapstoneBlghBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_BLGH_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBlghBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBlghBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Capstone_BLGH_BE/CapstoneBlghBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_BLGH_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBlghBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
