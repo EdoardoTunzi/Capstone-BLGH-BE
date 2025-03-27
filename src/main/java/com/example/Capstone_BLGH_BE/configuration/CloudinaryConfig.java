c5a0ea595560504481ed6490e137fb491f422d37
Edoardo <edfl.tunzi@gmail.com>
2025-03-27 17:33:59 +0100

    variabili d'ambiente aggiunte per Cloudinary

--- src/main/java/com/example/Capstone_BLGH_BE/configuration/CloudinaryConfig.java
+++ src/main/java/com/example/Capstone_BLGH_BE/configuration/CloudinaryConfig.java
@@ -13,9 +13,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary uploader() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dpsswabjf");
-        config.put("api_key", "694858762295978");
-        config.put("api_secret", "Pzmkzt8_K1kFUszpMgUnVWg97l8");
+        config.put("cloud_name", System.getenv("CLOUD_NAME"));
+        config.put("api_key", System.getenv("CLOUD_APIKEY"));
+        config.put("api_secret", System.getenv("CLOUD_API_SECRET"));
         return new Cloudinary(config);
     }
 }
